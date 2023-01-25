4e7522c3702b023d297c8f498e0b3248f54c3d37
Illari Alvarez-Gil <illari.alvarezgil@gmail.com>
2023-01-25 01:30:48 +0100

    barchart finished

--- main.js
+++ main.js
@@ -1,32 +1,48 @@
-// consts:
-const width = 800
-const height = 500
-const margin = {
-    top: 10,
-    right: 10,
-    left: 60,
-    bottom: 10,
-}
+const margin = { top: 20, right: 30, bottom: 40, left: 90 },
+    width = 1000 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-// groups:
-const svg = d3.select("#chart").append("svg").attr("id", "svg").attr("width", width).attr("height", height)
-const elementGroup = svg.append("g").attr("class", "elementGroup")
-const axisGroup = svg.append("g").attr("class", "axisGroup")
-const xAxisGroup = axisGroup.append("g").attr("class", "xAxisGroup").attr("transform", `translate(${margin.left}, ${height - margin.bottom - margin.top})`)
-const yAxisGroup = axisGroup.append("g").attr("class", "yAxisGroup").attr("transform", `translate(${margin.left}, ${0})`)
+const svg = d3.select("#bar_chart")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-// scales & axes:
+const barsGroup = svg.append("g").attr("class", "barsGroup");
+
+const axisGroup = svg.append("g").attr("class", "axisGroup");
+
+const xAxisGroup = axisGroup
+    .append("g").attr("class", "xAxisGroup")
+    .attr("transform", `translate(${margin.left}, ${height - margin.bottom - margin.top})`)
+
+const yAxisGroup = axisGroup
+    .append("g").attr("class", "yAxisGroup")
+    .attr("transform", `translate(${margin.left}, ${0})`)
+
+
 let x = d3.scaleLinear().range([0, width - margin.left - margin.right])
-let y = d3.scaleBand().range([height - margin.top - margin.bottom, 0]).padding(0.1)
+let y = d3.scaleBand().range([height - margin.top - margin.bottom, 0]).padding(.1)
 
 const xAxis = d3.axisBottom().scale(x).ticks(5)
 const yAxis = d3.axisLeft().scale(y)
+
+svg.append("text")
+    .attr("transform", "translate(" + (width / 2) + " ," + (height - 10) + ")")
+    .style("text-anchor", "middle")
+    .text("Won cups");
+
+svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -(height / 2))
+    .attr("y", 15)
+    .style("text-anchor", "middle")
+    .text("Country");
+
 
 let years;
 let winners;
 let originalData;
 
-// data:
 d3.csv("data.csv").then(data => {
     data.map(d => {
         d.year = +d.year
@@ -35,90 +51,72 @@ d3.csv("data.csv").then(data => {
 
     originalData = data
     years = data.map(d => +d.year)
-    winners = d3.nest()
-        .key(d => d.winner)
-        .entries(data)
-
+    winners = d3.groups(data, d => d.winner).map((value) => value = { key: value[0], values: value[1] })
     x.domain([0, d3.max(winners.map(d => d.values.length))])
     y.domain(winners.map(d => d.key))
 
     xAxisGroup.call(xAxis)
     yAxisGroup.call(yAxis)
 
-    // update:
-    update(winners)
+    updateData(winners)
     slider()
 })
 
-// update:
-function update(data) {
-    // identify top value:
-    let max = d3.max(data.map(d => d.values.length))
-    let elements = elementGroup.selectAll("rect").data(data)
-    
-    elements.enter()
-        .append("rect")
-            .attr("class", d => d.values.length == max ? "bar max" : "bar")
-            .attr("height", y.bandwidth())
-            .attr("x", margin.left)
-            .attr("y", d => y(d.key))
-            .transition()
-                .duration(300)
-            .attr("width", d => x(d.values.length))
-
-
-    elements
-    .attr("class", d => d.values.length == max ? "bar max" : "bar")
-    .attr("height", y.bandwidth())
-    .attr("x", margin.left)
-    .attr("y", d => y(d.key))
-    .transition()
-        .duration(300)
-    .attr("width", d => x(d.values.length))
-
-
-    elements.exit()
+const barsStructure = (bars, max) => {
+    return bars
+        .attr("class", d => d.values.length == max ? "bar max" : "bar")
+        .attr("height", y.bandwidth())
+        .attr("x", margin.left)
+        .attr("y", d => y(d.key))
         .transition()
-            .duration(100)
+        .duration(200)
+        .attr("width", d => x(d.values.length))
+}
+
+const updateData = (data) => {
+    let max = d3.max(data.map(d => d.values.length))
+    let bars = barsGroup.selectAll("rect").data(data)
+
+    barsStructure(bars.enter().append("rect"), max)
+    barsStructure(bars, max)
+
+    bars.exit()
+        .transition()
+        .duration(100)
         .attr("width", 0)
 }
 
-// treat data:
-function filterDataByYear(year) {
+const yearFilter = (year) => {
     let updatedData = originalData.filter(d => d.year <= year)
-    updatedData = d3.nest()
-        .key(d => d.winner)
-        .entries(updatedData)
-
+    updatedData = d3.groups(updatedData, d => d.winner)
+        .map((value) => value = { key: value[0], values: value[1] })
     return updatedData
 }
 
-
 // slider:
-function slider() {    
-    var sliderTime = d3
+const slider = () => {
+    const sliderTime = d3
         .sliderBottom()
-        .min(d3.min(years))
+        .min(d3.min(years))  // rango años
         .max(d3.max(years))
-        .step(4)
-        .width(580)
+        .step(4)  // cada cuánto aumenta el slider
+        .width(580)  // ancho de nuestro slider
         .ticks(years.length)
-        .default(years[years.length -1])
-        .on('onchange', val => {
-            d3.select('p#value-time').text(val);
-            update(filterDataByYear(val))
+        .default(years[years.length - 1])  // punto inicio de la marca
+        .on('onchange', value => {
+            d3.select("p#value-time").text(value);
+            updateData(yearFilter(value))
         });
 
-        var gTime = d3
-        .select('div#slider-time')
+    const gTime = d3
+        .select('div#slider-time')  // div donde lo insertamos
         .append('svg')
         .attr('width', width * 0.8)
         .attr('height', 100)
         .append('g')
         .attr('transform', 'translate(30,30)');
 
-        gTime.call(sliderTime);
+    gTime.call(sliderTime);
 
-        d3.select('p#value-time').text(sliderTime.value());
+    d3.select('p#value-time').text(sliderTime.value());
 }
-
